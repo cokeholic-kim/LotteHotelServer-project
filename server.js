f303c0b8cbdbdd337b9783374777448ff26c721e
donghyeon <kimadonghyon99@gmail.com>
2023-02-03 13:58:06 +0900

    server.js add join request , npm install bcrypt

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
 const multer = require("multer");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 //서버 생성
 const app = express();
 
+//포트번호
 const port = 8080;
+
 //브라우져의 cors이슈를 막기 위해 설정
 app.use(cors());
 
@@ -59,6 +63,32 @@ app.get("/special/:no",(req,res)=>{
     conn.query(`select * from event where e_category='special' and e_no = ${no}`,(err,result,field)=>{
         res.send(result)
     })
+})
+//회원가입요청
+app.post("/join",async (req,res)=>{
+    //입력받은 비밀번호를 mytextpass로 저장
+    const mytextpass = req.body.m_pass;
+    let myPass = ""
+    const {m_name,m_pass,m_phone,m_nickname,m_add1,m_add2,m_email} = req.body;
+
+    // 빈문자열이 아니고 undefined가 아닐때
+    if(mytextpass != '' && mytextpass != undefined){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            //hash메소드 호출되면 인자로 넣어준 비밀번호를 암호화 하여 콜백함수 안 hash로 돌려준다
+            bcrypt.hash(mytextpass, salt, function(err, hash) {// hash는 암호화시켜서 리턴되는값.
+                // Store hash in your password DB.
+                myPass = hash;
+                conn.query(`insert into member(m_name,m_pass,m_phone,m_nickname,m_address1,m_address2,m_email) 
+                values( '${m_name}' , '${myPass}' , '${m_phone}' , '${m_nickname}' , '${m_add1}' , '${m_add2}' , '${m_email}' )`
+                ,(err,result,fields)=>{
+                    console.log(result);
+                    res.send("등록되었습니다.")
+                })
+            
+            });
+        });
+    }
+    console.log(req.body)
 })
 
 app.listen(port,()=>{
